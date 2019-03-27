bf275c46ac2f9b36017893741a4f88d077e96a0b
ForbidenGod <45788548+ForbidenGod@users.noreply.github.com>
2019-03-27 16:18:36 -0300

    bootstrap

--- Sistema/src/app/app-routing.module.ts
+++ Sistema/src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {path: 'home', loadChildren: './login/login.module#LoginModule'},
-  { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
-  { path: '', pathMatch: 'full', redirectTo: '/home'},
-]
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    RouterModule.forRoot(routes, {useHash: true})
-  ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Sistema/src/app/app.module.ts
+++ Sistema/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({

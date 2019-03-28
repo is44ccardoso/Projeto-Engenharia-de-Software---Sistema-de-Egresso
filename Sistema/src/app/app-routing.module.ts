5395df4d922ed53a1c0502561b5459988f3385e8
ForbidenGod <45788548+ForbidenGod@users.noreply.github.com>
2019-03-28 13:35:41 -0300

    Uuser module

--- Sistema/src/app/app-routing.module.ts
+++ Sistema/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: './login/login.module#LoginModule'},
+  { path: 'home', loadChildren: './home/home.module#HomeModule'},
+  { path: '', pathMatch: 'full', redirectTo: '/home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
